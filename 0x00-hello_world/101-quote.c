440d301bb3af56d45f9b93214c33936133a7d88d
MUFTIATBAKARE <bakare.muftiat@gmail.com>
2022-12-08 18:56:52 -0800

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,9 @@
  * main - using write
  *
  * Return: 1
- *
  */
 int main(void)
 {	
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
