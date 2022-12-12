c2de9c7747cbcf30be482282604549b6f6b7b3f1
MUFTIATBAKARE <bakare.muftiat@gmail.com>
2022-12-12 15:49:56 -0800

    print numbers o-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints digits from 0-9.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int n;
+		for (n = 0; n < 10; n++)
+		{
+			printf("%d\n", n);
+		}
+
+	return (0);
+}
