e31cd1a43c83080755a1b6ef780f4b9c20208edb
MUFTIATBAKARE <bakare.muftiat@gmail.com>
2022-12-13 06:53:41 -0800

    rint two two-digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints two two-digits
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{	int x, y;
+	for (x = 0; x < 99; x++)
+	{
+		for (y = x + 1; y < 100; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 98 && y == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
