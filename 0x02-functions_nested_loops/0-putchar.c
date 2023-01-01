7bb42dd54ade9f8d117b605b8307504fb606c5d6
MUFTIATBAKARE <bakare.muftiat@gmail.com>
2022-12-31 18:31:42 -0800

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,14 @@
  */
 int main(void);
 {
-	int;
-
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	putchar('\n');
 
 	return (0);
